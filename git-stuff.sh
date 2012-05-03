4b67acedbea5579d66d1b3e5862e351224838df3
Adam Dunson <ich@v0gel.us>
2012-05-03 14:08:56 +0000

    Created a setup file and fixed all of the perms

--- git-stuff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if command -v git >/dev/null 2>&1; then
-  git config --global core.editor vim
-  git config --global user.name "Adam Dunson"
-  #git config --global user.email "ich@v0gel.us"
-  cp ./gitignore_global $HOME/.gitignore_global
-  git config --global core.excludesfile ~/.gitignore_global
-fi
---
+++ linux-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+dotfiles=(
+  bash_profile
+  bashrc
+  gitignore_global
+  gvimrc
+  nethackrc
+  vim
+  vimrc
+)
+
+# copy dat shit
+for f in $dotfiles; do
+  cp -R $f ../.$f
+done
+
+# clone the vim plugins
+cd $HOME/.vim/bundle/
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/hallison/vim-markdown.git
+
+# git configs
+git config --global core.editor vim
+git config --global user.name "Adam Dunson"
+git config --global core.excludesfile ~/.gitignore_global
+
+echo "Remember to set your git email like so:"
+echo
+echo "    git config --global user.email \"ich@v0gel.us\""
