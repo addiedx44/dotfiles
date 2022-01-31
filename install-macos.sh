1a640f4722b2e47447749e74acd9f001b37e5d8e
Addie Drake <addie@v0gel.io>
2022-01-30 21:56:18 -0500

    Fix some installs

--- install-macos.sh
+++ install-macos.sh
@@ -13,21 +13,13 @@ function paranoid_cp {
     fi
 }
 
-# oh-my-zsh
-if ! [ -d "$HOME/.oh-my-zsh" ]; then
-    echo 'Installing oh-my-zsh...'
-    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-paranoid_cp "$(dirname "$0")/zshrc" "$HOME/.zshrc"
-paranoid_cp -r "$(dirname "$0")/zsh_custom/*.zsh" "$HOME/.oh-my-zsh/custom"
-
-
 # homebrew
 if ! command -v brew ; then
     echo 'Installing homebrew...'
     xcode-select --install
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+eval "$(/opt/homebrew/bin/brew shellenv)"
 xargs brew install < "$(dirname "$0")/brew.txt"
 
 
@@ -59,13 +51,19 @@ paranoid_cp "$(dirname "$0")/gitconfig" "$HOME/.gitconfig"
 paranoid_cp "$(dirname "$0")/gitignore_global" "$HOME/.gitignore_global"
 
 
+# oh-my-zsh
+if ! [ -d "$HOME/.oh-my-zsh" ]; then
+    echo 'Installing oh-my-zsh...'
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+paranoid_cp -r "$(dirname "$0")/zsh_custom/*.zsh" "$HOME/.oh-my-zsh/custom"
+paranoid_cp -r "$(dirname "$0")/zshrc" "$HOME/.zshrc"
+paranoid_cp -r "$(dirname "$0")/zprofile" "$HOME/.zprofile"
+
 # p10k
 if ! [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ] ; then
     echo 'Installing p10k...'
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 fi
 paranoid_cp "$(dirname "$0")/p10k.zsh" "$HOME/.p10k.zsh"
-
-
-# finally, configure p10k
-p10k configure
+source "$HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme"
